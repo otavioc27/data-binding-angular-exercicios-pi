f72aa8cfdb261acf13e4be6da481437dea358db0
Otavio <274090@unifio.edu.br>
2026-09-21 00:13:33 -0300

    feat: resolve exercício 3

---
+++ exercicios/src/app/exercicios/exercicio3/exercicio3.ts
@@ -0,0 +1,3 @@
+import { Component } from '@angular/core';
+@Component({ selector: 'app-exercicio3', standalone: false, templateUrl: './exercicio3.html', styleUrl: './exercicio3.css' })
+export class Exercicio3 { imagemProduto = 'https://picsum.photos/400/250'; descricaoImagem = 'Imagem de produto'; }
--- exercicios/src/app/exercicios/exercicios-module.ts
+++ exercicios/src/app/exercicios/exercicios-module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { ExerciciosRoutingModule } from './exercicios-routing-module';
 import { Exercicio1 } from './exercicio1/exercicio1';
 import { Exercicio2 } from './exercicio2/exercicio2';
+import { Exercicio3 } from './exercicio3/exercicio3';
 
 @NgModule({
-  declarations: [Exercicio1, Exercicio2],
+  declarations: [Exercicio1, Exercicio2, Exercicio3],
   imports: [CommonModule, ExerciciosRoutingModule, FormsModule],
 })
 export class ExerciciosModule {}
